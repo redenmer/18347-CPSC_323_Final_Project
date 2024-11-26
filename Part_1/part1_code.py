a8abd2cfa734f0bec4de26ad4ae733a30527d267
Ian Martinez <b.ianmartinez98@gmail.com>
2024-11-26 14:28:39 -0800

    Part 1 Finished

--- Part_1/part1_code.py
+++ Part_1/part1_code.py
@@ -1,3 +1,16 @@
+# Group names: Ian Martinez, Kiara Guerra, Arturo Roman, Omar Montes
+# Final Project Part 1
+# Due date : 12/10/2024
+# Purpose: Applies the following rules to final.txt file and copies the new version in file “final24.txt”
+#          1.Any line/s or part of a line that begins with (* and ends with *) is/are considered as a
+#          comment line(s) (i.e. lines #2-3,5, 11,13,15), remove them all
+#
+#          2.Any blank lines must be removed (i.e. lines #12, 16)
+#
+#          3.Extra spaces in each line must be removed, leaving one space before and one after each token
+#          to make tokenization easier
+
+# Read lines beginning and ending with "(*" & "*) as comments and removes them entirely
 def remove_comments(s):
     while '(*' in s and '*)' in s:
         start = s.find('(*')
@@ -5,6 +18,7 @@ def remove_comments(s):
         s = s[:start] + s[end:]
     return s
 
+# Reads each line from final.txt and removes any line that is empty
 def remove_empty_lines_and_excessive_spacing(s):
     # Remove empty lines
     lines = s.split('\n')
@@ -15,14 +29,15 @@ def remove_empty_lines_and_excessive_spacing(s):
 
     return '\n'.join(cleaned_lines)
 
-# Read the content of the file "final.txt"
+# Read the content of the file "final.txt" as a string under utf-8 format
+# so that tokens: " can be read
 with open("final.txt", "r", encoding="utf-8") as x:
     content = x.read()
 
-# Clean the content
+# Cleaning process of the file
 cleaned = remove_comments(content)
 cleaned = remove_empty_lines_and_excessive_spacing(cleaned)
 
-# Print the cleaned content
+# Print the cleaned content in utf-8 format so each token can be printed
 with open("final24.txt", "w", encoding="utf-8") as f:
     f.write(cleaned)
